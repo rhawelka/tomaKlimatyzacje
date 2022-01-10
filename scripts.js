e5e5aca3bbe664ca98a4526968361827634043ea
QPWC84 <Rafal.Hawelka@motorolasolutions.com>
2022-01-11 00:13:34 +0100

    fix minheight image

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ console.log(dupa)
 function handleSubmit() {
     console.log("witajcie u tomy klima")
 }
+
+// GOOGLE MAPS
+// Initialize and add the map
+function initMap() {
+    const gliwiceLocation = { lat: 50.307611, lng: 18.663559 }
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: gliwiceLocation
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: gliwiceLocation,
+        map: map,
+    });
+}
