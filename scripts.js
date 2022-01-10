25e846452d83414dc3dc6c4cbc6a7f805156668c
rhawelka <rafalhawelka@gmail.com>
2022-01-11 00:54:28 +0100

    Feature add footer (#1)
    * working responsive footer
    
    * fix minheight image
    
    * footer v1
    
    * add bg
    
    Co-authored-by: QPWC84 <Rafal.Hawelka@motorolasolutions.com>

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 let dupa = "dupa"
 
 console.log(dupa)
+
+function handleSubmit() {
+    console.log("witajcie u tomy klima")
+    alert("siema Tomo")
+}
+
+/*************   GOOGLE MAPS  ***********/
+// Initialize and add the map
+function initMap() {
+    const gliwiceLocation = { lat: 50.307611, lng: 18.663559 }
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: gliwiceLocation
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: gliwiceLocation,
+        map: map,
+    });
+}
